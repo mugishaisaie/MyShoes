b435936cc85e1e41a20b629279d0efc3291250ff
Mugisha Isaie <izerimanaadeline2@gmail.com>
2026-02-06 16:27:28 +0200

    removed every file to start over

--- sanity/schemas/index.tsx
+++
@@ -1,6 +0,0 @@
-import { product } from './product'
-import { category } from './category'
-import { brand } from './brand'
-import { storeSettings } from './storeSettings'
-
-export const schemaTypes = [product, category, brand, storeSettings]
