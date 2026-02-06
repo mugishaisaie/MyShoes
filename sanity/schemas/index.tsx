a3299386e46add3ddded0f0c0b5a176cd448a786
Mugisha Isaie <izerimanaadeline2@gmail.com>
2026-02-06 15:52:25 +0200

    implementing backend with sanity

---
+++ sanity/schemas/index.tsx
@@ -0,0 +1,6 @@
+import { product } from './product'
+import { category } from './category'
+import { brand } from './brand'
+import { storeSettings } from './storeSettings'
+
+export const schemaTypes = [product, category, brand, storeSettings]
